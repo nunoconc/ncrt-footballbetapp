88161f4db0aacd1377e836fe46914d175440e1af
nunoconc <nmpconceicao@gmail.com>
2019-11-04 23:42:10 +0000

    added project spring boot for web server

---
+++ footballbetappserver/src/main/java/com/ncrt/apps/server/footballbetappserver/FootballbetappserverApplication.java
@@ -0,0 +1,13 @@
+package com.ncrt.apps.server.footballbetappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballbetappserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballbetappserverApplication.class, args);
+	}
+
+}
---
+++ footballbetappserver/src/test/java/com/ncrt/apps/server/footballbetappserver/FootballbetappserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncrt.apps.server.footballbetappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballbetappserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
